beb5722fc99f6fcd240c709643815957235e2144
sunclouds <254033430@qq.com>
2017-03-01 17:33:08 +0800

    some init msg

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 刘哲 on 2017/3/1.
+//  Copyright © 2017年 刘哲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by 刘哲 on 2017/3/1.
+//  Copyright © 2017年 刘哲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
